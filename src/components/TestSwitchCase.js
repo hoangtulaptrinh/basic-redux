ba93aac925efb5d2b2cdfd73b1120409fb3a47c4
Do Tung Duong <duong98kk@gmail.com>
2019-09-19 14:25:26 +0700

    'aaaaa'

--- src/components/TestSwitchCase.js
+++ src/components/TestSwitchCase.js
@@ -9,16 +9,16 @@ class Yui_Hatano extends Component{
         this.onClick222 = this.onClick222.bind(this);
       }
 onClick111(){
-    this.props.nhapchuot99()
+    this.props.Plus1000()
 }
 onClick222(){
-    this.props.aaaaaa99()
+    this.props.Plus2000()
 }
     render(){
         return<div>
-        <button onClick = { this.onClick111 } >Button111</button>
-        <h1>{this.props.hienthi999}</h1>
-        <button onClick = { this.onClick222 } >Button111</button>
+        <button onClick = { this.onClick111 } >Button + 1000</button>
+        <h1>{this.props.hienthiprops}</h1>
+        <button onClick = { this.onClick222 } >Button + 2000</button>
         </div>
     }
 }
@@ -26,14 +26,14 @@ onClick222(){
 //chuyển state HandleClick của store chuyển thành props có tên là hienthi
 const mapStatetoProps = (state) => {
     return {
-        hienthi999 : state.Plus
+        hienthiprops : state.Plus
     }
   }
   //chuyển hàm dispatch action thành props có tên là nhapchuot
   const mapDispatchToProps = (dispatch,props) => {
     return {
-      nhapchuot99 : () =>  dispatch(actions.Plus1000()) ,
-      aaaaaa99: () => dispatch(actions.Plus2000())
+      Plus1000 : () =>  dispatch(actions.Plus1000()) ,
+      Plus2000: () => dispatch(actions.Plus2000())
     }
   }
   
